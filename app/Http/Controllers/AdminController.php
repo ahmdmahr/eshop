bb86d8067766a8a72a9240be92883c1c1e60c599
Ahamd Maher <ahmadmaher.work@gmail.com>
2024-10-26 21:28:26 +0300

    Cleanup the admin dashboard from useless stuff

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index(){
-        return view('backend.index');
+        $categories = Category::where('status','active')->get();
+        $products = Product::where('status','active')->get();
+        $customers = User::where(['status'=>'active','role'=>'customer'])->get();
+        $orders = Order::get();
+        return view('backend.index',compact('categories','products','customers','orders'));
     }
 }
